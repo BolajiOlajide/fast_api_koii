29d07ccff23dd38190a0453445100cf11e6e38ad
zachang <zachangdawuda@gmail.com>
2021-10-01 13:01:27 +0100

    make ANSI escape characters readable

--- koii.py
+++ koii.py
@@ -1,5 +1,6 @@
 from typing import List, NamedTuple, Set
 
+from colorama import Fore
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
 from starlette.routing import BaseRoute
@@ -14,7 +15,7 @@ class Path(NamedTuple):
 
 
 class Koii(object):
-    _VALID_METHODS: List[str] = ["GET", "POST", "DELETE", "PUT", "PATCH"]
+    _VALID_METHODS: Set[str] = frozenset(["GET", "POST", "DELETE", "PUT", "PATCH"])
     _TAB_SIZE: int = 12
 
     def __init__(self, app: FastAPI) -> None:
@@ -25,18 +26,16 @@ class Koii(object):
             raise Exception("FastAPI app isn't valid as it has no predefined routes.")
 
     def _is_app_valid(self, app: FastAPI) -> bool:
-        if app is not None and app.routes:
-            for route in app.routes:
-                if isinstance(route, APIRoute):
-                    return True
+        for route in app.routes:
+            if isinstance(route, APIRoute):
+                return True
 
     def _get_paths(self, routes: KOIIRoutes) -> List[Path]:
         paths = []
 
         for route in routes:
-            path = route.path
             for valid_method in self._get_valid_methods(route.methods):
-                paths.append(Path(path, valid_method))
+                paths.append(Path(route.path, valid_method))
 
         return paths
 
@@ -48,17 +47,17 @@ class Koii(object):
         path_header_fill = " " * (self._TAB_SIZE - len("PATH"))
 
         print(" ")
-        method_header = f"\033[1;35;40mMETHOD {method_header_fill}"
-        path_header = f"\033[1;35;40mPATH {path_header_fill}"
+        method_header = f"{Fore.LIGHTMAGENTA_EX} METHOD {method_header_fill}"
+        path_header = f"{Fore.LIGHTMAGENTA_EX}PATH {path_header_fill}"
 
         print(f"{method_header} {path_header}")
         print(("-" * self._TAB_SIZE) + " " + ("-" * self._TAB_SIZE))
 
         for path in paths:
-            method_fill_str = " " * (self._TAB_SIZE - len(path.method))
+            method_fill_str = " " * (self._TAB_SIZE - len(path.method) - 2)
 
-            method_to_print = f"\033[1;32;40m{path.method} {method_fill_str}"
-            path_to_print = f"\033[0;37;40m{path.path}"
+            method_to_print = f"{Fore.GREEN} {path.method} {method_fill_str}"
+            path_to_print = f"{Fore.CYAN} {path.path}"
 
             print(method_to_print + " " + path_to_print)
         print(" ")
